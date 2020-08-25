24db092e652dcc9e1a8e683038deb5f79f797e86
Luke <39779310+ClashLuke@users.noreply.github.com>
2020-08-26 00:02:23 +0200

    feat(preparation): use rocksdb
    about 4x faster than lmdb (write) while removing the in-memory limitation and adding highly advanced compression and caching.

--- prepare_data.py
+++ prepare_data.py
@@ -1,13 +1,18 @@
+#!/bin/bash
 import argparse
 import multiprocessing
 from functools import partial
 from io import BytesIO
 
-import lmdb
+import rocksdb
 from PIL import Image
 from torchvision import datasets
 from torchvision.transforms import functional as trans_fn
 import time
+
+# WARNING: The code is expected to use 16 GiB just as DB cache. 
+# If you don't have this much RAM available, make sure to decrease the buffer variables
+# below (write_buffer_size, block_cache, block_cache_compressed).
 
 PRINTERVALL = 1024
 
@@ -22,39 +27,26 @@ def resize_worker(img_file, sizes):
     return img_file[0], [resize_and_convert(img, size) for size in sizes]
 
 
-def open_db(name):
-    def _make():
-        return lmdb.open(name, map_size=1e12, readahead=False, writemap=True)
-    return _make
-
-def prepare(out, dataset, n_worker, sizes=(128, 256, 512, 1024), buffer=64):
+def prepare(db, dataset, n_worker, sizes=(128, 256, 512, 1024), buffer=64):
     resize_fn = partial(resize_worker, sizes=sizes)
     image_count = str(len(dataset))
     files = [(i, file) for i, (file, label) in enumerate(sorted(dataset.imgs, key=lambda x: x[0]), 1)]
     start = time.time()
-    make_db = open_db(out)
-    db = make_db()
-    txn = db.begin(write=True)
-
+    
     with multiprocessing.Pool(n_worker) as pool:
         for i, imgs in pool.imap_unordered(resize_fn, files):
             for size, img in zip(sizes, imgs):
-                txn.put(f'{size}-{i:05d}'.encode('utf-8'), img)
+                db.put(f'{size}-{i:05d}'.encode('utf-8'), img)
             if i%PRINTERVALL == 0:
                 print(f"\r[{i:{len(image_count)}d}/{image_count}] Rate: {i/(time.time()-start):.2f} Img/s", end='')
-            if i%buffer == 0:
-                txn.commit()
-                db.close()
-                db = make_db()
-                txn = db.begin(write=True)
-
-        txn.put('length'.encode('utf-8'), str(i + 1).encode('utf-8'))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    sizes = (64 * 2 ** i * (j + 2) for i in range(4) for j in range(2))
+    sizes = [s for s in sizes if s <= 1024]
     parser.add_argument('--out', type=str, default="out.lmdb")
-    parser.add_argument('--size', type=str, default=','.join(map(str, (64 * 2 ** i * (j + 2) for i in range(4) for j in range(2)))))
+    parser.add_argument('--size', type=str, default=','.join(map(str, sizes)))
     parser.add_argument('--n_worker', type=int, default=12)
     parser.add_argument('path', type=str)
 
@@ -66,4 +58,18 @@ if __name__ == '__main__':
 
     imgset = datasets.ImageFolder(args.path)
 
-    prepare(args.out, imgset, args.n_worker, sizes=sizes)
+    opts = rocksdb.Options(create_if_missing=True, 
+                           write_buffer_size=2**32,
+                           max_write_buffer_number=2**14,
+                           max_open_files=-1,
+                           target_file_size_base=2**26,
+                           target_file_size_multiplier=4,
+                           max_bytes_for_level_base=2**28,
+                           manifest_preallocation_size=2**36,
+                           table_factory=rocksdb.BlockBasedTableFactory(filter_policy=rocksdb.BloomFilterPolicy(10),
+                                                                        block_cache=rocksdb.LRUCache(2 ** 33),
+                                                                        block_cache_compressed=rocksdb.LRUCache(2 ** 32)
+                                                                        )
+                           )
+    db = rocksdb.DB("out.rdb", opts)
+    prepare(db, imgset, args.n_worker, sizes=sizes)
