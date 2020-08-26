ab2720902fc81985c9e39456961a146e73fdc2a0
Luke <39779310+ClashLuke@users.noreply.github.com>
2020-08-26 02:12:44 +0200

    fix(prepare): fix memory issue
    I _assume_ that by moving it out it just isnt broken anymore. No idea though.

--- prepare_data.py
+++ prepare_data.py
@@ -1,6 +1,6 @@
-#!/bin/bash
 import argparse
 import multiprocessing
+import time
 from functools import partial
 from io import BytesIO
 
@@ -8,13 +8,13 @@ import rocksdb
 from PIL import Image
 from torchvision import datasets
 from torchvision.transforms import functional as trans_fn
-import time
-
-# WARNING: The code is expected to use 16 GiB just as DB cache. 
-# If you don't have this much RAM available, make sure to decrease the buffer variables
-# below (write_buffer_size, block_cache, block_cache_compressed).
 
 PRINTERVALL = 1024
+
+# WARNING: The code is expected to use 32 GiB (it never goes above 3.4GB) just as DB cache. 
+# If you don't have this much RAM available, make sure to decrease the buffer variables
+# below (write_buffer_size, block_cache, block_cache_compressed). 
+# Changing it to 8GB reduces the speed by ~3%.
 
 def resize_and_convert(img, size):
     buffer = BytesIO()
@@ -32,13 +32,13 @@ def prepare(db, dataset, n_worker, sizes=(128, 256, 512, 1024), buffer=64):
     image_count = str(len(dataset))
     files = [(i, file) for i, (file, label) in enumerate(sorted(dataset.imgs, key=lambda x: x[0]), 1)]
     start = time.time()
-    
+
     with multiprocessing.Pool(n_worker) as pool:
         for i, imgs in pool.imap_unordered(resize_fn, files):
             for size, img in zip(sizes, imgs):
                 db.put(f'{size}-{i:05d}'.encode('utf-8'), img)
-            if i%PRINTERVALL == 0:
-                print(f"\r[{i:{len(image_count)}d}/{image_count}] Rate: {i/(time.time()-start):.2f} Img/s", end='')
+            if i % PRINTERVALL == 0:
+                print(f"\r[{i:{len(image_count)}d}/{image_count}] Rate: {i / (time.time() - start):.2f} Img/s", end='')
 
 
 if __name__ == '__main__':
@@ -57,19 +57,18 @@ if __name__ == '__main__':
     print(f'Make dataset of image sizes:', ', '.join(str(s) for s in sizes))
 
     imgset = datasets.ImageFolder(args.path)
-
-    opts = rocksdb.Options(create_if_missing=True, 
-                           write_buffer_size=2**32,
-                           max_write_buffer_number=2**14,
-                           max_open_files=-1,
-                           target_file_size_base=2**26,
-                           target_file_size_multiplier=4,
-                           max_bytes_for_level_base=2**28,
-                           manifest_preallocation_size=2**36,
-                           table_factory=rocksdb.BlockBasedTableFactory(filter_policy=rocksdb.BloomFilterPolicy(10),
-                                                                        block_cache=rocksdb.LRUCache(2 ** 33),
-                                                                        block_cache_compressed=rocksdb.LRUCache(2 ** 32)
-                                                                        )
-                           )
+    opts = rocksdb.Options()
+    opts.create_if_missing = True
+    opts.write_buffer_size = 2 ** 34
+    opts.max_write_buffer_number = 2 ** 14
+    opts.max_open_files = -1
+    opts.compression = rocksdb.CompressionType.no_compression
+    opts.target_file_size_base = 2 ** 26
+    opts.target_file_size_multiplier = 4
+    opts.max_bytes_for_level_base = 2 ** 28
+    opts.manifest_preallocation_size = 2 ** 36
+    opts.table_factory = rocksdb.BlockBasedTableFactory(filter_policy=rocksdb.BloomFilterPolicy(32),
+                                                        block_cache=rocksdb.LRUCache(2 ** 34, 32),
+                                                        )
     db = rocksdb.DB("out.rdb", opts)
     prepare(db, imgset, args.n_worker, sizes=sizes)
